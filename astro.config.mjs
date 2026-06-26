264d6ce473217fd9eef0cf699086c37a60d92812
Shayne-max <shfanfood@163.com>
2026-06-26 15:30:57 +0800

    Add Cloudflare adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://dogguidehub.com",
+  integrations: [sitemap()],
+  output: "server",
+  adapter: cloudflare(),
 });
